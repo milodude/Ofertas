c478c6ea6d3b0a4688d7994f035d6fc07f5fac4c
Matias Raverta <mraverta@nuba.com.ar>
2022-05-04 14:51:36 -0300

    Handled no sharedPreferences found for userData

--- lib/features/login/data/data_sources/google_sign_in_account_local_data_source.dart
+++ lib/features/login/data/data_sources/google_sign_in_account_local_data_source.dart
@@ -1,25 +1,25 @@
 import 'dart:convert';
 
 import 'package:ofertas/core/constants/constants.dart';
+import 'package:ofertas/core/error/exceptions.dart';
 import 'package:ofertas/features/login/data/models/google_sign_in_account_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:ofertas/features/login/domain/entities/user_data.dart';
 
 //Esto habria que ver si sirve o no.
-abstract class GoogleSignInAccountLocalDataSource{
+abstract class GoogleSignInAccountLocalDataSource {
   ///Gets the last used account from Google
   Future<UserData> getGoogleAccount();
+
   ///Sets the cache with the used google account the user selected to log in into the app with.
   Future<void> cacheGoogleAccount(UserData modelToCache);
 }
 
-class GoogleSignInAccountLocalDataSourceImpl implements GoogleSignInAccountLocalDataSource {
-
+class GoogleSignInAccountLocalDataSourceImpl
+    implements GoogleSignInAccountLocalDataSource {
   final SharedPreferences sharedPreferences;
-  GoogleSignInAccountLocalDataSourceImpl(
-    {required this.sharedPreferences}
-  );
+  GoogleSignInAccountLocalDataSourceImpl({required this.sharedPreferences});
 
   @override
   Future<void> cacheGoogleAccount(UserData modelToCache) {
@@ -30,8 +30,12 @@ class GoogleSignInAccountLocalDataSourceImpl implements GoogleSignInAccountLocal
   @override
   Future<UserData> getGoogleAccount() {
     final account = sharedPreferences.getString(googleSignInData);
-    return Future.value(GoogleSignInAccountModel.fromJson(json.decode(account!)).toUserData());
+    if (account != null) {
+      return Future.value(
+          GoogleSignInAccountModel.fromJson(json.decode(account))
+              .toUserData());
+    }else{
+      throw CachedException('No value in sharedPreferences found for userData');
+    }
   }
-
-
 }
--- test/features/login/data/data_sources/google_sign_in_account_local_data_source_test.dart
+++ test/features/login/data/data_sources/google_sign_in_account_local_data_source_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:ofertas/core/constants/constants.dart';
+import 'package:ofertas/core/error/exceptions.dart';
 import 'package:ofertas/features/login/data/data_sources/google_sign_in_account_local_data_source.dart';
 import 'package:ofertas/features/login/data/models/google_sign_in_account_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -31,6 +32,18 @@ void main() {
       //Assert
       verify(mockSharedPreferences.getString(googleSignInData));
       expect(result, equals(tGoogleSignInData.toUserData()));
+    });
+
+    testWidgets('should return a cacheException if having saved previously any value',
+        (tester) async {
+      //Arrange
+      when(mockSharedPreferences.getString(any)).thenReturn(
+          (null));
+      //Act
+      //Setea en call la funcion sin ser ejecutada. Se evalua la ejecucion en el expect
+      final call = localDataSourceImpl.getGoogleAccount;
+      //Assert
+      expect(()=>call(), equals(throwsA(const TypeMatcher<CachedException>())));
     });
 
     //TODO: crear test para setear el valor en shared preferences
